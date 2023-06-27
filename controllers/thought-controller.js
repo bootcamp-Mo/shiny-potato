4708f4e6a508f6e8107c5b242f7a5b228974265f
Monique <mo.sherred@gmail.com>
2023-06-27 14:44:40 -0700

    how show thought when message is sent

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -5,7 +5,7 @@ module.exports = {
 	async getAllThoughts(req, res) {
 		try {
 			const thoughts = await Thought.find()
-			res.status(200).json({ message: 'Found all thoughts' })
+			res.status(200).json(thoughts)
 		} catch (error) {
 			res.status(500).json(error)
 		}
